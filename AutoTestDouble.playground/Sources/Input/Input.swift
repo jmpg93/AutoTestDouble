941faa94b9db7581b48430711993012644069913
José María Puerta <jmpg93@gmail.com>
2019-02-12 20:11:55 +0100

    Update empty

--- AutoTestDouble.playground/Sources/Input/Input.swift
+++ AutoTestDouble.playground/Sources/Input/Input.swift
@@ -1,11 +1,3 @@
 import Foundation
 
-// Mock
-protocol ChannelRepositoryProtocol {
-	var channels: [String] { get }
-	var brandName: String? { get set }
-
-	func getChannels()
-	func markAsDisabled(channelId: String)
-	func markAsEnabled(channelId: String)
-}
+// Paste your protocol here.
